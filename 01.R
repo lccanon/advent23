212af42cbdcf7aa21c9658bd2df66eba982a037a
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-12-02 05:56:29 +0100

    Day 1

---
+++ 01.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+read_lines("input01") %>%
+  str_replace("(one|two|three|four|five|six|seven|eight|nine)", "\\1 ") %>%
+  str_replace("(.*)(one|two|three|four|five|six|seven|eight|nine)", "\\1\\2 ") %>%
+  str_replace_all("one ", "1") %>%
+  str_replace_all("two ", "2") %>%
+  str_replace_all("three ", "3") %>%
+  str_replace_all("four ", "4") %>%
+  str_replace_all("five ", "5") %>%
+  str_replace_all("six ", "6") %>%
+  str_replace_all("seven ", "7") %>%
+  str_replace_all("eight ", "8") %>%
+  str_replace_all("nine ", "9") %>%
+  str_extract_all("\\d") %>%
+  map_int(~ str_c(head(., 1), tail(., 1)) %>% as.numeric) %>%
+  sum

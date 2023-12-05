7d1e1b3cd50b115c1e2791842c85676ea68ca2c7
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2023-12-05 10:58:43 +0100

    Clarification

--- 04.R
+++ 04.R
@@ -7,14 +7,10 @@ input %>%
   str_split(" \\| ") %>%
   map(~ str_extract_all(., "\\d+")) -> cards
 
-winning <- list()
-numbers <- list()
 score <- 0
 copy <- rep(1, length(cards))
 for (i in 1:length(cards)) {
-  winning[[i]] <- as.numeric(cards[[i]][[1]])
-  numbers[[i]] <- as.numeric(cards[[i]][[2]])
-  gain <- sum(numbers[[i]] %in% winning[[i]])
+  gain <- sum(cards[[i]][[1]] %in% cards[[i]][[2]])
   if (gain > 0)
     score <- score + 2^(gain - 1)
   copy[i + seq_len(gain)] <- copy[i + seq_len(gain)] + copy[i]
